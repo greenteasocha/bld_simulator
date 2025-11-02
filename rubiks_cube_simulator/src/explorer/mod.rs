7827ade754d1e0a22245f77da6074dd5094d3da2
greenteasocha <kouheiatts@gmail.com>
2025-11-03 02:43:29 +0900

    refactor: 統合されて不要になった exproler を削除

--- rubiks_cube_simulator/src/explorer/mod.rs
+++ rubiks_cube_simulator/src/explorer/mod.rs
@@ -1,32 +1,12 @@
-pub mod nearby_search;
-pub mod wrong_operation_detector;
-pub mod modifier;
-pub mod edge_modifier;
-pub mod edge_nearby_search;
-pub mod edge_wrong_operation_detector;
-pub mod mixed_nearby_search;
-pub mod mixed_modifier;
 pub mod alternative_generator;
+pub mod mixed_modifier;
+pub mod mixed_nearby_search;
 
-pub use nearby_search::NearbyOperationSearch;
-pub use wrong_operation_detector::WrongOperationDetector;
-pub use modifier::{SwapModifier, TwistModifier, CornerModifier, ModifiedSequence};
-pub use edge_modifier::{EdgeSwapModifier, EdgeFlipModifier, EdgeModifier, ModifiedEdgeSequence};
-pub use edge_nearby_search::NearbyEdgeOperationSearch;
-pub use edge_wrong_operation_detector::WrongEdgeOperationDetector;
-pub use mixed_nearby_search::{
-    MixedOperation, 
-    NearbyMixedOperationSearch, 
-    AlternativeGenerator,
-    ApplyableToState,
-};
-pub use mixed_modifier::{
-    ModifiedMixedSequence,
-    MixedModifier,
-};
 pub use alternative_generator::{
-    CornerSwapAlternativeGenerator,
+    CornerSwapAlternativeGenerator, CornerTwistAlternativeGenerator, EdgeFlipAlternativeGenerator,
     EdgeSwapAlternativeGenerator,
-    CornerTwistAlternativeGenerator,
-    EdgeFlipAlternativeGenerator,
+};
+pub use mixed_modifier::{MixedModifier, ModifiedMixedSequence};
+pub use mixed_nearby_search::{
+    AlternativeGenerator, ApplyableToState, MixedOperation, NearbyMixedOperationSearch,
 };
--- rubiks_cube_simulator/src/lib.rs
+++ rubiks_cube_simulator/src/lib.rs
@@ -1,23 +1,20 @@
+pub mod cli_state_input;
 pub mod cube;
 pub mod display;
-pub mod inspection;
 pub mod explorer;
+pub mod inspection;
 pub mod parser;
-pub mod cli_state_input;
 pub mod workflow;
 
-pub use cube::{State, RubiksCube, PartialStatePattern, SolutionSearcher, Move};
-pub use display::{CubeDisplay, CubeNetWidget, CubeColor, Face, StateToDisplay};
-pub use inspection::{
-    CornerSwapOperation, CornerTwistOperation, CornerOperation, CornerInspection,
-    EdgeSwapOperation, EdgeFlipOperation, EdgeOperation, EdgeInspection,
-    MoveSequence, OperationsToTurns
-};
-pub use explorer::{
-    NearbyOperationSearch, WrongOperationDetector, SwapModifier, TwistModifier, CornerModifier, ModifiedSequence,
-    EdgeSwapModifier, EdgeFlipModifier, EdgeModifier, ModifiedEdgeSequence,
-    NearbyEdgeOperationSearch, WrongEdgeOperationDetector
-};
-pub use parser::{NotationMove, Sequence, parse_sequence, sequence_to_string, parse_and_expand, parse_3style_csv};
 pub use cli_state_input::StateInputEditor;
-pub use workflow::{BldWorkflow};
+pub use cube::{Move, PartialStatePattern, RubiksCube, SolutionSearcher, State};
+pub use display::{CubeColor, CubeDisplay, CubeNetWidget, Face, StateToDisplay};
+pub use inspection::{
+    CornerInspection, CornerOperation, CornerSwapOperation, CornerTwistOperation,
+    EdgeFlipOperation, EdgeInspection, EdgeOperation, EdgeSwapOperation, MoveSequence,
+    OperationsToTurns,
+};
+pub use parser::{
+    parse_3style_csv, parse_and_expand, parse_sequence, sequence_to_string, NotationMove, Sequence,
+};
+pub use workflow::BldWorkflow;
